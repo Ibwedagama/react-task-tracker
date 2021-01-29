46a10d45f48b762b567b789eb275e0ca0f19ed22
Ida Bagus Wedagama <wardana.wedagama@gmail.com>
2021-01-29 19:47:21 +0800

    [ibw] Http request POST PUT DELETE

--- src/App.js
+++ src/App.js
@@ -11,14 +11,20 @@ function App() {
 
 	useEffect(() => {
 		const getTasks = async () => {
-			const fetchDataFromServer = await fetchTask()
+			const fetchDataFromServer = await fetchTasks()
 			setTasks(fetchDataFromServer)
 		}
 		getTasks()
 	}, [])
 
-	const fetchTask = async () => {
+	const fetchTasks = async () => {
 		const res = await fetch('http://localhost:5000/tasks')
+		const data = await res.json()
+		return data
+	}
+
+	const fetchTask = async (id) => {
+		const res = await fetch(`http://localhost:5000/tasks/${id}`)
 		const data = await res.json()
 		return data
 	}
@@ -45,8 +51,19 @@ function App() {
 		setTasks(tasks.filter((task) => task.id !== id))
 	}
 
-	const toggleReminder = (id) => {
-		setTasks(tasks.map((task) => (task.id === id ? { ...task, reminder: !task.reminder } : task)))
+	const toggleReminder = async (id) => {
+		const taskToToggle = await fetchTask(id)
+		const updatedTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
+		const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+			method: 'PUT',
+			headers: {
+				'Content-type': 'Application/json',
+			},
+			body: JSON.stringify(updatedTask),
+		})
+		const data = await res.json()
+
+		setTasks(tasks.map((task) => (task.id === id ? { ...task, reminder: data.reminder } : task)))
 	}
 
 	const toggleAddTask = () => {
